00d5d2a9614880d33958d49dd4f53d25d0724c89
Sergei <sergei.by4kov.93@mail.ru>
2024-10-07 16:27:40 +0300

    Выполнены расчёта площади и периметра треугольника и выполнены тесты

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -2,8 +2,7 @@ package ru.stqa.geometry;
 
 import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
-
-import static java.lang.Math.sqrt;
+import ru.stqa.geometry.figures.Triangle;
 
 public class Geometry {
     public static void main(String[] args) {
@@ -14,22 +13,9 @@ public class Geometry {
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(7.0, 9.0);
 
-        printTriangleArea (2.0, 3.0, 4.0);
+        Triangle.printTriangleArea (3.0, 4.0, 5.0);
     
     }
-    static void printTriangleArea (double a, double b, double c) {
-        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " и "  + c + " = " + TriangleArea(a, b, c));
-
-        System.out.println("Периметр треугольника со сторонами " + a + " и " + b + " и "  + c + " = " + TrianglePerimeter(a, b, c));
-
-    }
-
-    private static double TrianglePerimeter(double a, double b, double c) {
-        return (a+b+c);
-    }
 
 
-    private static double TriangleArea(double a, double b, double c) {
-        return sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
-    }
 }
--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,2 +1,22 @@
 package ru.stqa.geometry.figures;
 
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+    public static void printTriangleArea(double a, double b, double c) {
+        String text1 = String.format("Площадь треугольника со сторонами %f и %f и %f = %f", a, b, c, TriangleArea(a, b, c));
+        System.out.println(text1);
+
+        String text2 = String.format("Периметр треугольника со сторонами %f и %f и %f = %f", a, b, c, TrianglePerimeter(a, b, c));
+        System.out.println(text2);
+
+    }
+
+    private static double TrianglePerimeter(double a, double b, double c) {
+        return (a+b+c);
+    }
+
+    public static double TriangleArea(double a, double b, double c) {
+        return sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
+    }
+}
